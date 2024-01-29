0a7e739c0a0f8d893cecd4e895cb9592e4df8c8c
Docam-cod <nesterov.andrey-nester2017@yandex.ru>
2024-01-30 00:39:22 +0300

    Add Two Command

--- GitExample/Program.cs
+++ GitExample/Program.cs
@@ -10,6 +10,10 @@ namespace GitExample
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+
         }
     }
 }
